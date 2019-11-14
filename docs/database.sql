7ca929ef36c881a58ccccaa808edf5f4d95d758d
Benefrancis <benefrancis@gmail.com>
2019-11-13 23:19:47 -0300

    Testando o servidor de autenticação com o postman

---
+++ docs/database.sql
@@ -0,0 +1,203 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.8-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura para tabela authdb.oauth_access_token
+CREATE TABLE IF NOT EXISTS `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` mediumblob DEFAULT NULL,
+  `authentication_id` varchar(256) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` mediumblob DEFAULT NULL,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_access_token: ~0 rows (aproximadamente)
+DELETE FROM `oauth_access_token`;
+/*!40000 ALTER TABLE `oauth_access_token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_access_token` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.oauth_approvals
+CREATE TABLE IF NOT EXISTS `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `lastModifiedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_approvals: ~0 rows (aproximadamente)
+DELETE FROM `oauth_approvals`;
+/*!40000 ALTER TABLE `oauth_approvals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_approvals` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.oauth_client_details
+CREATE TABLE IF NOT EXISTS `oauth_client_details` (
+  `client_id` varchar(255) NOT NULL,
+  `client_secret` varchar(255) NOT NULL,
+  `web_server_redirect_uri` varchar(2048) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `resource_ids` varchar(1024) DEFAULT NULL,
+  `authorized_grant_types` varchar(1024) DEFAULT NULL,
+  `authorities` varchar(1024) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_client_details: ~1 rows (aproximadamente)
+DELETE FROM `oauth_client_details`;
+/*!40000 ALTER TABLE `oauth_client_details` DISABLE KEYS */;
+INSERT INTO `oauth_client_details` (`client_id`, `client_secret`, `web_server_redirect_uri`, `scope`, `access_token_validity`, `refresh_token_validity`, `resource_ids`, `authorized_grant_types`, `authorities`, `additional_information`, `autoapprove`) VALUES
+	('mobile', '{bcrypt}$2a$10$DXV6Dk7X9vNr02T9jaF9MeonKJ7DuE8klc2p5fj7ivCSJtFgMt4B.', 'http://localhost:8080/code', 'READ,WRITE', 3600, 10000, 'inventory,payment', 'authorization_code,password,refresh_token,implicit', NULL, '{}', NULL);
+/*!40000 ALTER TABLE `oauth_client_details` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.oauth_client_token
+CREATE TABLE IF NOT EXISTS `oauth_client_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` mediumblob DEFAULT NULL,
+  `authentication_id` varchar(256) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_client_token: ~0 rows (aproximadamente)
+DELETE FROM `oauth_client_token`;
+/*!40000 ALTER TABLE `oauth_client_token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_client_token` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.oauth_code
+CREATE TABLE IF NOT EXISTS `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_code: ~0 rows (aproximadamente)
+DELETE FROM `oauth_code`;
+/*!40000 ALTER TABLE `oauth_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_code` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.oauth_refresh_token
+CREATE TABLE IF NOT EXISTS `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` mediumblob DEFAULT NULL,
+  `authentication` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.oauth_refresh_token: ~5 rows (aproximadamente)
+DELETE FROM `oauth_refresh_token`;
+/*!40000 ALTER TABLE `oauth_refresh_token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth_refresh_token` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.permission
+CREATE TABLE IF NOT EXISTS `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.permission: ~4 rows (aproximadamente)
+DELETE FROM `permission`;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+INSERT INTO `permission` (`id`, `name`) VALUES
+	(1, 'create_profile'),
+	(4, 'delete_profile'),
+	(2, 'read_profile'),
+	(3, 'update_profile');
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.permission_role
+CREATE TABLE IF NOT EXISTS `permission_role` (
+  `permission_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  KEY `permission_id` (`permission_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `permission_role_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`),
+  CONSTRAINT `permission_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.permission_role: ~6 rows (aproximadamente)
+DELETE FROM `permission_role`;
+/*!40000 ALTER TABLE `permission_role` DISABLE KEYS */;
+INSERT INTO `permission_role` (`permission_id`, `role_id`) VALUES
+	(1, 1),
+	(2, 1),
+	(3, 1),
+	(4, 1),
+	(2, 2),
+	(3, 2);
+/*!40000 ALTER TABLE `permission_role` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.role: ~2 rows (aproximadamente)
+DELETE FROM `role`;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`id`, `name`) VALUES
+	(1, 'ROLE_admin'),
+	(2, 'ROLE_operator');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.role_user
+CREATE TABLE IF NOT EXISTS `role_user` (
+  `role_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  KEY `role_id` (`role_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.role_user: ~3 rows (aproximadamente)
+DELETE FROM `role_user`;
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+INSERT INTO `role_user` (`role_id`, `user_id`) VALUES
+	(1, 1);
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela authdb.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(1024) NOT NULL,
+  `email` varchar(1024) NOT NULL,
+  `enabled` tinyint(4) NOT NULL,
+  `accountNonExpired` tinyint(4) NOT NULL,
+  `credentialsNonExpired` tinyint(4) NOT NULL,
+  `accountNonLocked` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela authdb.user: ~3 rows (aproximadamente)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `enabled`, `accountNonExpired`, `credentialsNonExpired`, `accountNonLocked`) VALUES
+	(1, 'francis', '{bcrypt}$2a$10$DXV6Dk7X9vNr02T9jaF9MeonKJ7DuE8klc2p5fj7ivCSJtFgMt4B.', 'benefrancis@gmail.com', 1, 1, 1, 1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
